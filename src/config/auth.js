be3600564d406651175e767621e96bc11c2e12ab
Everton Kunz <ehokeverton@gmail.com>
2019-09-12 20:32:45 -0300

    Auth with JWT feat added.

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '3a8b5ae5c7259f37b9d6809a9b37c2db',
+  expiresIn: '7d'
+}
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,18 @@
 import { Router } from 'express'
+
+// Our Controllers
 import UserController from './app/controllers/UserController'
+import SessionController from './app/controllers/SessionController'
+
+// Middlewares
+import authMiddleware from './app/middlewares/auth'
 
 const routes = new Router()
 
 routes.post('/users', UserController.store)
+routes.post('/sessions', SessionController.store)
+// Rotas com autenticação
+routes.use(authMiddleware)
+routes.put('/users', UserController.update)
 
 export default routes
