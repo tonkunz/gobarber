4a82a63b7975e8fc8eac16849f42c5de4e57ef28
Everton Kunz <ehokeverton@gmail.com>
2019-09-12 03:09:28 -0300

    DB Class, Models loader & UserController added.

--- src/database/migrations/20190911054842-create-users.js
+++ src/database/migrations/20190911054842-create-users.js
@@ -11,8 +11,7 @@ module.exports = {
       },
       name: {
         type: Sequelize.STRING,
-        allowNull: false,
-        unique: true
+        allowNull: false
       },
       email: {
         type: Sequelize.STRING,
