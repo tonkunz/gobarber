d59aed342420f1dba8dc431fdb42d02b78d2f9f0
Everton Kunz <ehokeverton@gmail.com>
2019-09-13 15:21:07 -0300

    Input data validation with Yup.

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,9 +1,21 @@
 import User from '../models/User'
 import jwt from 'jsonwebtoken'
 import authConfig from '../../config/auth'
+import * as Yup from 'yup'
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .required()
+        .email(),
+      password: Yup.string().required()
+    })
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' })
+    }
+
     const { email, password } = req.body
 
     const user = await User.findOne({ where: { email } })
