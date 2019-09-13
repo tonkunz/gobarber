d1b15440b7befa5eb102601a9ca7d753fe4c023b
Everton Kunz <ehokeverton@gmail.com>
2019-09-12 22:28:28 -0300

    Changes for UserController: Update method added.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -19,9 +19,31 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId) // userId commes from authMiddleware
+    const { email, oldPassword } = req.body
 
-    return res.json({ ok: true })
+    const user = await User.findByPk(req.userId)
+
+    // Verificação caso esteja alterando o email
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } })
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' })
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' })
+    }
+
+    const { id, name, provider } = await user.update(req.body)
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider
+    })
   }
 }
 
