3de7b4fb737f0f9895a644c9c0d162a2ae86b185
Everton Kunz <ehokeverton@gmail.com>
2019-09-24 23:41:31 -0300

    AppointmentController:Method index with pagination added.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,39 @@
 import Appointment from '../models/Appointment'
 import { startOfHour, parseISO, isBefore } from 'date-fns'
 import User from '../models/User'
+import File from '../models/File'
 import * as Yup from 'yup'
 
 class AppointmentController {
+  async index(req, res) {
+    const { page = 1 } = req.query
+
+    const appointments = await Appointment.findAll({
+      wher: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      // Paginação
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url']
+            }
+          ]
+        }
+      ]
+    })
+
+    return res.json(appointments)
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
--- src/routes.js
+++ src/routes.js
@@ -26,5 +26,6 @@ routes.get('/providers', ProviderController.index)
 routes.post('/files', upload.single('file'), FileController.store)
 // Appointment Routes
 routes.post('/appointments', AppointmentController.store)
+routes.get('/appointments', AppointmentController.index)
 
 export default routes
