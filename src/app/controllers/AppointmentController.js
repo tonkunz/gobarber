1ecd2e82a4c2f7fc4df1c301243cd26f1c603d9a
Everton Kunz <ehokeverton@gmail.com>
2019-09-26 22:53:05 -0300

    Store notifications feature added in AppointmentController.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,11 @@
+import { startOfHour, parseISO, isBefore, format } from 'date-fns'
+import pt from 'date-fns/locale/pt'
+import * as Yup from 'yup'
+
 import Appointment from '../models/Appointment'
-import { startOfHour, parseISO, isBefore } from 'date-fns'
 import User from '../models/User'
 import File from '../models/File'
-import * as Yup from 'yup'
+import Notification from '../schemas/Notification'
 
 class AppointmentController {
   async index(req, res) {
@@ -81,6 +84,19 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date
+    })
+
+    // Notify appointment provider
+    const user = await User.findByPk(req.userId)
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    )
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id
     })
 
     return res.json(appointment)
