fb3722626cf7b89b8cdfe0b2fa27046976c85300
Everton Kunz <ehokeverton@gmail.com>
2019-09-28 20:19:23 -0300

    Email notifing of appointment cancel with nodemailer added.

---
+++ src/config/mail.js
@@ -0,0 +1,13 @@
+// Credentials
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '47e4a812c7fea7',
+    pass: 'ca677a440abe4e'
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>'
+  }
+}
