a6b15fccf4c4f2eeef0e81fc2dd4c116136ac8ff
Everton Kunz <ehokeverton@gmail.com>
2019-10-01 01:24:53 -0300

    AvailableController for listing avaialable times added.

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ import ProviderController from './app/controllers/ProviderController'
 import AppointmentController from './app/controllers/AppointmentController'
 import ScheduleController from './app/controllers/ScheduleController'
 import NotificationController from './app/controllers/NotificationController'
+import AvailableController from './app/controllers/AvailableController'
 
 // Middlewares
 import authMiddleware from './app/middlewares/auth'
@@ -24,6 +25,7 @@ routes.post('/sessions', SessionController.store)
 routes.use(authMiddleware)
 routes.put('/users', UserController.update)
 routes.get('/providers', ProviderController.index)
+routes.get('/providers/:providerId/available', AvailableController.index)
 // Upload de arquivos
 routes.post('/files', upload.single('file'), FileController.store)
 // Appointment Routes
